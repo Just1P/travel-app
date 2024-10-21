034049fc00b899d0b78be80418cf1b308e2371db
Pitra <justin.pitra@gmail.com>
2024-10-21 12:00:07 +0200

    Setup our Routing

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,18 +1,21 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 import TravelListPage from "./pages/TravelListPage";
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <TravelListPage />,
-  },
-]);
+import TravelSinglePage from "./pages/TravelSinglePage";
 
 function App() {
   return (
-    <div>
-      <RouterProvider router={router} />
-    </div>
+    <BrowserRouter>
+      <nav className="flex justify-between items-center p-4 bg-red-400 text-white">
+        <Link to="/">Home</Link>
+      </nav>
+
+      <div className="container mx-auto mt-10">
+        <Routes>
+          <Route path="/" element={<TravelListPage />} />
+          <Route path="/:id" element={<TravelSinglePage />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
--- app/src/components/CardTravel.tsx
+++ app/src/components/CardTravel.tsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import { TravelType } from "../types/travel.type";
 
 type CardTravelProps = {
@@ -10,8 +11,11 @@ const CardTravel = ({ travel }: CardTravelProps) => {
       <div className="h-[200px] overflow-hidden">
         <img src={travel.image} alt="" />
       </div>
-      <div className="p-2">
-        <h2 className="text-xl text-zinc-500 mt-2">{travel.name}</h2>
+      <div className="p-6">
+        <Link to={`/$travel.id}`}>
+          <h2 className="text-xl text-zinc-500 mt-2">{travel.name}</h2>
+        </Link>
+
         <p>
           {travel.description.substring(0, 49)}
           {travel.description.length > 49 && "..."}
